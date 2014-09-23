a2501bf122dcc87410d17b3fb0c2c605f0a89d7f
Sindre Sorhus <sindresorhus@gmail.com>
2014-09-23 16:16:18 +0200

    tweaks

--- index.js
+++ index.js
@@ -73,12 +73,7 @@ module.exports = function (url, opts, cb) {
 
 			// pipe the response to the proxy if in proxy mode
 			if (proxy) {
-				res.pipe(proxy);
-
-				res.on('error', function forwardError(error) {
-					proxy.emit('error', error);
-				})
-
+				res.on('error', proxy.emit.bind(proxy, 'error')).pipe(proxy);
 				return;
 			}
 
