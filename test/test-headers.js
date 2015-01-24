768158bd29e5e62d683780328bacd4f24a5f5c9e
Vsevolod Strukchinsky <floatdrop@gmail.com>
2015-01-25 00:39:42 +0500

    Add test on Host header
    Closes #36

--- test/test-headers.js
+++ test/test-headers.js
@@ -14,11 +14,26 @@ tape('setup', function (t) {
 	});
 });
 
-tape('send user-agent and accept-encoding by default', function (t) {
+tape('send user-agent header by default', function (t) {
+	got(s.url, function (err, data) {
+		var headers = JSON.parse(data);
+		t.equal(headers['user-agent'], 'https://github.com/sindresorhus/got');
+		t.end();
+	});
+});
+
+tape('send accept-encoding header by default', function (t) {
 	got(s.url, function (err, data) {
 		var headers = JSON.parse(data);
 		t.equal(headers['accept-encoding'], 'gzip,deflate');
-		t.equal(headers['user-agent'], 'https://github.com/sindresorhus/got');
+		t.end();
+	});
+});
+
+tape('send host header by default', function (t) {
+	got(s.url, function (err, data) {
+		var headers = JSON.parse(data);
+		t.equal(headers.host, 'localhost:' + s.port);
 		t.end();
 	});
 });
