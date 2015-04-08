7da072b60898269490a1d3ef27a1c1ddad0c30a3
Vsevolod Strukchinsky <floatdrop@gmail.com>
2015-04-08 12:49:02 +0500

    Parse non-200 responses when options.json is true
    Closes #46

--- test/test-json.js
+++ test/test-json.js
@@ -12,6 +12,15 @@ s.on('/invalid', function (req, res) {
 	res.end('/');
 });
 
+s.on('/non200', function (req, res) {
+	res.statusCode = 500;
+	res.end('{"data":"dog"}');
+});
+
+s.on('/non200-invalid', function (req, res) {
+	res.statusCode = 500;
+	res.end('Internal error');
+});
 
 tape('setup', function (t) {
 	s.listen(s.port, function () {
@@ -38,6 +47,22 @@ tape('json option wrap parsing errors', function (t) {
 	got(s.url + '/invalid', {json: true}, function (err) {
 		t.ok(err);
 		t.equal(err.message, 'Parsing ' + s.url + '/invalid response failed');
+		t.end();
+	});
+});
+
+tape('json option should parse non-200 responses', function (t) {
+	got(s.url + '/non200', {json: true}, function (err, json) {
+		t.ok(err);
+		t.deepEqual(json, {data: 'dog'});
+		t.end();
+	});
+});
+
+tape('json option should catch errors on invalid non-200 responses', function (t) {
+	got(s.url + '/non200-invalid', {json: true}, function (err, json) {
+		t.ok(err);
+		t.deepEqual(json, 'Internal error');
 		t.end();
 	});
 });
