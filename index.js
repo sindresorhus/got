b6de56528174bb7748e112239186879aee91189e
Tom Andrews <Tom.Andrews@barclaycard.co.uk>
2016-01-20 10:21:11 +0000

    Basic proxy support
    Added really basic proxy support. It works for the simple use case of
    got("reddit.com") which will first make a http request and then be
    redirected to https. This also supports requests that are redirected and
    over to unsecure locations i.e. they start http and stay http.
    
    Currently requests that require https to begin with are not supported.

--- index.js
+++ index.js
@@ -19,319 +19,375 @@ const nodeStatusCodes = require('node-status-codes');
 const isPlainObj = require('is-plain-obj');
 const isRetryAllowed = require('is-retry-allowed');
 
+const shouldProxy = !!process.env.HTTP_PROXY;
+const parsedProxy = shouldProxy ? urlParseLax(process.env.HTTP_PROXY) : {};
+
 function requestAsEventEmitter(opts) {
-	opts = opts || {};
+  opts = opts || {};
 
-	const ee = new EventEmitter();
-	let redirectCount = 0;
-	let retryCount = 0;
+  const ee = new EventEmitter();
+  let redirectCount = 0;
+  let retryCount = 0;
 
-	const get = opts => {
-		const fn = opts.protocol === 'https:' ? https : http;
+  const get = opts => {
+    const fn = opts.protocol === 'https:' ? https : http;
+    const req = fn.request(opts, res => {
+      const statusCode = res.statusCode;
 
-		const req = fn.request(opts, res => {
-			const statusCode = res.statusCode;
+      if (isRedirect(statusCode) && 'location' in res.headers && (opts.method === 'GET' || opts.method === 'HEAD')) {
+        res.resume();
 
-			if (isRedirect(statusCode) && 'location' in res.headers && (opts.method === 'GET' || opts.method === 'HEAD')) {
-				res.resume();
+        if (++redirectCount > 10) {
+          ee.emit('error', new got.MaxRedirectsError(statusCode, opts), null, res);
+          return;
+        }
 
-				if (++redirectCount > 10) {
-					ee.emit('error', new got.MaxRedirectsError(statusCode, opts), null, res);
-					return;
-				}
+        const redirectUrl = urlLib.resolve(urlLib.format(opts), res.headers.location);
+        const parsedRedirect = urlLib.parse(redirectUrl);
+        let redirectOpts = Object.assign({}, opts, parsedRedirect);
+        if (shouldProxy) {
+          if (parsedRedirect.protocol === 'https:') {
+						const connectOptions = { // establishing a tunnel
+              host: parsedProxy.hostname,
+              port: parsedProxy.port,
+              method: 'CONNECT',
+              path: `${parsedRedirect.hostname}:443`,
+              headers: {
+                'Proxy-Authorization': 'Basic ' + new Buffer(parsedProxy.auth).toString('base64'),
+              }
+            };
+            http.request(connectOptions).on('connect', function(res, socket, head) {
+              socket.on('error', function() {
+                console.log('Socket error', arguments)
+              });
 
-				const redirectUrl = urlLib.resolve(urlLib.format(opts), res.headers.location);
-				const redirectOpts = Object.assign({}, opts, urlLib.parse(redirectUrl));
+              redirectOpts = Object.assign({}, parsedRedirect, {
+                socket,
+								agent: false,
+              });
+              ee.emit('redirect');
+							get(redirectOpts);
+            }).end();
+            return;
+          } else {
+            redirectOpts = Object.assign({}, opts, {
+              path: redirectUrl,
+              headers: Object.assign({}, opts.headers, {
+                Host: parsedRedirect.host
+              })
+            });
+          }
+        }
+        ee.emit('redirect', res, redirectOpts);
+        get(redirectOpts);
 
-				ee.emit('redirect', res, redirectOpts);
+        return;
+      }
 
-				get(redirectOpts);
-				return;
-			}
+      setImmediate(() => {
+        ee.emit('response', typeof unzipResponse === 'function' && req.method !== 'HEAD' ? unzipResponse(res) : res);
+      });
+    });
 
-			setImmediate(() => {
-				ee.emit('response', typeof unzipResponse === 'function' && req.method !== 'HEAD' ? unzipResponse(res) : res);
-			});
-		});
+    req.once('error', err => {
+      const backoff = opts.retries(++retryCount, err);
 
-		req.once('error', err => {
-			const backoff = opts.retries(++retryCount, err);
+      if (backoff) {
+        setTimeout(get, backoff, opts);
+        return;
+      }
 
-			if (backoff) {
-				setTimeout(get, backoff, opts);
-				return;
-			}
+      ee.emit('error', new got.RequestError(err, opts));
+    });
 
-			ee.emit('error', new got.RequestError(err, opts));
-		});
+    if (opts.timeout) {
+      timedOut(req, opts.timeout);
+    }
 
-		if (opts.timeout) {
-			timedOut(req, opts.timeout);
-		}
+    setImmediate(() => ee.emit('request', req));
+  };
 
-		setImmediate(() => ee.emit('request', req));
-	};
-
-	get(opts);
-	return ee;
+  get(opts);
+  return ee;
 }
 
 function asPromise(opts) {
-	return new Promise((resolve, reject) => {
-		const ee = requestAsEventEmitter(opts);
+  return new Promise((resolve, reject) => {
+    const ee = requestAsEventEmitter(opts);
 
-		ee.on('request', req => {
-			if (isStream(opts.body)) {
-				opts.body.pipe(req);
-				opts.body = undefined;
-				return;
-			}
+    ee.on('request', req => {
+      if (isStream(opts.body)) {
+        opts.body.pipe(req);
+        opts.body = undefined;
+        return;
+      }
 
-			req.end(opts.body);
-		});
+      req.end(opts.body);
+    });
 
-		ee.on('response', res => {
-			const stream = opts.encoding === null ? getStream.buffer(res) : getStream(res, opts);
+    ee.on('response', res => {
+      const stream = opts.encoding === null ? getStream.buffer(res) : getStream(res, opts);
 
-			stream
-				.catch(err => reject(new got.ReadError(err, opts)))
-				.then(data => {
-					const statusCode = res.statusCode;
+      stream
+        .catch(err => reject(new got.ReadError(err, opts)))
+        .then(data => {
+          const statusCode = res.statusCode;
 
-					res.body = data;
+          res.body = data;
 
-					if (opts.json && statusCode !== 204) {
-						try {
-							res.body = JSON.parse(res.body);
-						} catch (e) {
-							throw new got.ParseError(e, opts, data);
-						}
-					}
+          if (opts.json && statusCode !== 204) {
+            try {
+              res.body = JSON.parse(res.body);
+            } catch (e) {
+              throw new got.ParseError(e, opts, data);
+            }
+          }
 
-					if (statusCode < 200 || statusCode > 299) {
-						throw new got.HTTPError(statusCode, opts);
-					}
+          if (statusCode < 200 || statusCode > 299) {
+            throw new got.HTTPError(statusCode, opts);
+          }
 
-					resolve(res);
-				})
-				.catch(err => {
-					Object.defineProperty(err, 'response', {value: res});
-					reject(err);
-				});
-		});
+          resolve(res);
+        })
+        .catch(err => {
+          Object.defineProperty(err, 'response', {
+            value: res
+          });
+          reject(err);
+        });
+    });
 
-		ee.on('error', reject);
-	});
+    ee.on('error', reject);
+  });
 }
 
 function asStream(opts) {
-	const input = new PassThrough();
-	const output = new PassThrough();
-	const proxy = duplexer3(input, output);
+  const input = new PassThrough();
+  const output = new PassThrough();
+  const proxy = duplexer3(input, output);
 
-	if (opts.json) {
-		throw new Error('got can not be used as stream when options.json is used');
-	}
+  if (opts.json) {
+    throw new Error('got can not be used as stream when options.json is used');
+  }
 
-	if (opts.body) {
-		proxy.write = () => {
-			throw new Error('got\'s stream is not writable when options.body is used');
-		};
-	}
+  if (opts.body) {
+    proxy.write = () => {
+      throw new Error('got\'s stream is not writable when options.body is used');
+    };
+  }
 
-	const ee = requestAsEventEmitter(opts);
+  const ee = requestAsEventEmitter(opts);
 
-	ee.on('request', req => {
-		proxy.emit('request', req);
+  ee.on('request', req => {
+    proxy.emit('request', req);
 
-		if (isStream(opts.body)) {
-			opts.body.pipe(req);
-			return;
-		}
+    if (isStream(opts.body)) {
+      opts.body.pipe(req);
+      return;
+    }
 
-		if (opts.body) {
-			req.end(opts.body);
-			return;
-		}
+    if (opts.body) {
+      req.end(opts.body);
+      return;
+    }
 
-		if (opts.method === 'POST' || opts.method === 'PUT' || opts.method === 'PATCH') {
-			input.pipe(req);
-			return;
-		}
+    if (opts.method === 'POST' || opts.method === 'PUT' || opts.method === 'PATCH') {
+      input.pipe(req);
+      return;
+    }
 
-		req.end();
-	});
+    req.end();
+  });
 
-	ee.on('response', res => {
-		const statusCode = res.statusCode;
+  ee.on('response', res => {
+    const statusCode = res.statusCode;
 
-		res.pipe(output);
+    res.pipe(output);
 
-		if (statusCode < 200 || statusCode > 299) {
-			proxy.emit('error', new got.HTTPError(statusCode, opts), null, res);
-			return;
-		}
+    if (statusCode < 200 || statusCode > 299) {
+      proxy.emit('error', new got.HTTPError(statusCode, opts), null, res);
+      return;
+    }
 
-		proxy.emit('response', res);
-	});
+    proxy.emit('response', res);
+  });
 
-	ee.on('redirect', proxy.emit.bind(proxy, 'redirect'));
+  ee.on('redirect', proxy.emit.bind(proxy, 'redirect'));
 
-	ee.on('error', proxy.emit.bind(proxy, 'error'));
+  ee.on('error', proxy.emit.bind(proxy, 'error'));
 
-	return proxy;
+  return proxy;
 }
 
 function normalizeArguments(url, opts) {
-	if (typeof url !== 'string' && typeof url !== 'object') {
-		throw new Error(`Parameter \`url\` must be a string or object, not ${typeof url}`);
-	}
+  if (typeof url !== 'string' && typeof url !== 'object') {
+    throw new Error(`Parameter \`url\` must be a string or object, not ${typeof url}`);
+  }
 
-	if (typeof url === 'string') {
-		url = urlParseLax(url);
+  if (typeof url === 'string') {
+    url = urlParseLax(url);
 
-		if (url.auth) {
-			throw new Error('Basic authentication must be done with auth option');
-		}
-	}
+    if (url.auth) {
+      throw new Error('Basic authentication must be done with auth option');
+    }
+  }
 
-	opts = Object.assign(
-		{protocol: 'http:', path: '', retries: 5},
-		url,
-		opts
-	);
+  opts = Object.assign({
+      protocol: 'http:',
+      path: '',
+      retries: 5
+    },
+    url,
+    opts
+  );
 
-	opts.headers = Object.assign({
-		'user-agent': 'https://github.com/sindresorhus/got',
-		'accept-encoding': 'gzip,deflate'
-	}, lowercaseKeys(opts.headers));
+  opts.headers = Object.assign({
+    'user-agent': 'https://github.com/sindresorhus/got',
+    'accept-encoding': 'gzip,deflate'
+  }, lowercaseKeys(opts.headers));
 
-	const query = opts.query;
+  const query = opts.query;
 
-	if (query) {
-		if (typeof query !== 'string') {
-			opts.query = querystring.stringify(query);
-		}
+  if (query) {
+    if (typeof query !== 'string') {
+      opts.query = querystring.stringify(query);
+    }
 
-		opts.path = `${opts.path.split('?')[0]}?${opts.query}`;
-		delete opts.query;
-	}
+    opts.path = `${opts.path.split('?')[0]}?${opts.query}`;
+    delete opts.query;
+  }
 
-	if (opts.json && opts.headers.accept === undefined) {
-		opts.headers.accept = 'application/json';
-	}
+  if (opts.json && opts.headers.accept === undefined) {
+    opts.headers.accept = 'application/json';
+  }
 
-	let body = opts.body;
+  let body = opts.body;
 
-	if (body) {
-		if (typeof body !== 'string' && !Buffer.isBuffer(body) && !isStream(body) && !isPlainObj(body)) {
-			throw new Error('options.body must be a ReadableStream, string, Buffer or plain Object');
-		}
+  if (body) {
+    if (typeof body !== 'string' && !Buffer.isBuffer(body) && !isStream(body) && !isPlainObj(body)) {
+      throw new Error('options.body must be a ReadableStream, string, Buffer or plain Object');
+    }
 
-		opts.method = opts.method || 'POST';
+    opts.method = opts.method || 'POST';
 
-		if (isPlainObj(body)) {
-			opts.headers['content-type'] = opts.headers['content-type'] || 'application/x-www-form-urlencoded';
-			body = opts.body = querystring.stringify(body);
-		}
+    if (isPlainObj(body)) {
+      opts.headers['content-type'] = opts.headers['content-type'] || 'application/x-www-form-urlencoded';
+      body = opts.body = querystring.stringify(body);
+    }
 
-		if (opts.headers['content-length'] === undefined && opts.headers['transfer-encoding'] === undefined && !isStream(body)) {
-			const length = typeof body === 'string' ? Buffer.byteLength(body) : body.length;
-			opts.headers['content-length'] = length;
-		}
-	}
+    if (opts.headers['content-length'] === undefined && opts.headers['transfer-encoding'] === undefined && !isStream(body)) {
+      const length = typeof body === 'string' ? Buffer.byteLength(body) : body.length;
+      opts.headers['content-length'] = length;
+    }
+  }
 
-	opts.method = opts.method || 'GET';
+  opts.method = opts.method || 'GET';
 
-	if (opts.hostname === 'unix') {
-		const matches = /(.+)\:(.+)/.exec(opts.path);
+  if (opts.hostname === 'unix') {
+    const matches = /(.+)\:(.+)/.exec(opts.path);
 
-		if (matches) {
-			opts.socketPath = matches[1];
-			opts.path = matches[2];
-			opts.host = null;
-		}
-	}
+    if (matches) {
+      opts.socketPath = matches[1];
+      opts.path = matches[2];
+      opts.host = null;
+    }
+  }
 
-	if (typeof opts.retries !== 'function') {
-		const retries = opts.retries;
-		opts.retries = function backoff(iter, err) {
-			if (iter > retries || !isRetryAllowed(err)) {
-				return 0;
-			}
+  if (typeof opts.retries !== 'function') {
+    const retries = opts.retries;
+    opts.retries = function backoff(iter, err) {
+      if (iter > retries || !isRetryAllowed(err)) {
+        return 0;
+      }
 
-			const noise = Math.random() * 100;
-			return (1 << iter) * 1000 + noise;
-		};
-	}
+      const noise = Math.random() * 100;
+      return (1 << iter) * 1000 + noise;
+    };
+  }
 
-	return opts;
+  if (shouldProxy) {
+    opts = Object.assign(opts, {
+      host: parsedProxy.hostname,
+      hostname: parsedProxy.hostname,
+      path: opts.href,
+      port: parsedProxy.port,
+      headers: Object.assign({}, {
+        "Host": opts.hostname,
+        "Proxy-Authorization": "Basic " + new Buffer(parsedProxy.auth).toString("base64")
+      })
+    });
+  }
+  return opts;
 }
 
 function got(url, opts) {
-	try {
-		return asPromise(normalizeArguments(url, opts));
-	} catch (error) {
-		return Promise.reject(error);
-	}
+  try {
+    return asPromise(normalizeArguments(url, opts));
+  } catch (error) {
+    return Promise.reject(error);
+  }
 }
 
 const helpers = [
-	'get',
-	'post',
-	'put',
-	'patch',
-	'head',
-	'delete'
+  'get',
+  'post',
+  'put',
+  'patch',
+  'head',
+  'delete'
 ];
 
 helpers.forEach(el => {
-	got[el] = (url, opts) => got(url, Object.assign({}, opts, {method: el.toUpperCase()}));
+  got[el] = (url, opts) => got(url, Object.assign({}, opts, {
+    method: el.toUpperCase()
+  }));
 });
 
-got.stream = function (url, opts) {
-	return asStream(normalizeArguments(url, opts));
+got.stream = function(url, opts) {
+  return asStream(normalizeArguments(url, opts));
 };
 
 helpers.forEach(el => {
-	got.stream[el] = function (url, opts) {
-		return got.stream(url, Object.assign({}, opts, {method: el.toUpperCase()}));
-	};
+  got.stream[el] = function(url, opts) {
+    return got.stream(url, Object.assign({}, opts, {
+      method: el.toUpperCase()
+    }));
+  };
 });
 
 function stdError(error, opts) {
-	if (error.code !== undefined) {
-		this.code = error.code;
-	}
+  if (error.code !== undefined) {
+    this.code = error.code;
+  }
 
-	Object.assign(this, {
-		message: error.message,
-		host: opts.host,
-		hostname: opts.hostname,
-		method: opts.method,
-		path: opts.path
-	});
+  Object.assign(this, {
+    message: error.message,
+    host: opts.host,
+    hostname: opts.hostname,
+    method: opts.method,
+    path: opts.path
+  });
 }
 
 got.RequestError = createErrorClass('RequestError', stdError);
 got.ReadError = createErrorClass('ReadError', stdError);
-got.ParseError = createErrorClass('ParseError', function (e, opts, data) {
-	stdError.call(this, e, opts);
-	this.message = `${e.message} in "${urlLib.format(opts)}": \n${data.slice(0, 77)}...`;
+got.ParseError = createErrorClass('ParseError', function(e, opts, data) {
+  stdError.call(this, e, opts);
+  this.message = `${e.message} in "${urlLib.format(opts)}": \n${data.slice(0, 77)}...`;
 });
 
-got.HTTPError = createErrorClass('HTTPError', function (statusCode, opts) {
-	stdError.call(this, {}, opts);
-	this.statusCode = statusCode;
-	this.statusMessage = nodeStatusCodes[this.statusCode];
-	this.message = `Response code ${this.statusCode} (${this.statusMessage})`;
+got.HTTPError = createErrorClass('HTTPError', function(statusCode, opts) {
+  stdError.call(this, {}, opts);
+  this.statusCode = statusCode;
+  this.statusMessage = nodeStatusCodes[this.statusCode];
+  this.message = `Response code ${this.statusCode} (${this.statusMessage})`;
 });
 
-got.MaxRedirectsError = createErrorClass('MaxRedirectsError', function (statusCode, opts) {
-	stdError.call(this, {}, opts);
-	this.statusCode = statusCode;
-	this.statusMessage = nodeStatusCodes[this.statusCode];
-	this.message = 'Redirected 10 times. Aborting.';
+got.MaxRedirectsError = createErrorClass('MaxRedirectsError', function(statusCode, opts) {
+  stdError.call(this, {}, opts);
+  this.statusCode = statusCode;
+  this.statusMessage = nodeStatusCodes[this.statusCode];
+  this.message = 'Redirected 10 times. Aborting.';
 });
 
 module.exports = got;
