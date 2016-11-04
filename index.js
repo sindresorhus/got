db1ef7d8f62147e000079af065c47929a5d3957a
Tommy Troy Lin <tommytroylin@gmail.com>
2016-11-04 14:13:38 +0800

    format code

--- index.js
+++ index.js
@@ -40,12 +40,8 @@ function requestAsEventEmitter(opts) {
 					ee.emit('error', new got.MaxRedirectsError(statusCode, opts), null, res);
 					return;
 				}
-				const isNode4 = process.version
-					? process.version.indexOf('v4.') === 0
-					: true; 
-				const bufferString = isNode4
-					? new Buffer(res.headers.location, 'binary').toString()
-					: Buffer.from(res.headers.location, 'binary').toString();
+				const isNode4 = process.version? process.version.indexOf('v4.') === 0 : true;
+				const bufferString = isNode4 ? new Buffer(res.headers.location, 'binary').toString() : Buffer.from(res.headers.location, 'binary').toString();
 				redirectUrl = urlLib.resolve(urlLib.format(opts), bufferString);
 				const redirectOpts = Object.assign({}, opts, urlLib.parse(redirectUrl));
 
