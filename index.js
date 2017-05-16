dbb7f206e5aa588fe06f8d0fef0ee78c8b86790a
Luke Childs <lukechilds123@gmail.com>
2017-05-16 17:07:20 +0700

    Cache stream responses

--- index.js
+++ index.js
@@ -208,7 +208,24 @@ function asStream(opts) {
 	ee.on('response', res => {
 		const statusCode = res.statusCode;
 
-		res.pipe(output);
+		const policy = new CachePolicy(opts, res);
+		if (opts.cache && policy.storable()) {
+			const bodyChunks = [];
+			res.on('data', chunk => {
+				output.push(chunk);
+				bodyChunks.push(chunk);
+			});
+			res.on('end', () => {
+				output.push(null);
+				res.body = Buffer.concat(bodyChunks);
+				if (opts.encoding !== null) {
+					res.body = res.body.toString(opts.encoding);
+				}
+				cacheResponse(res, policy, opts);
+			});
+		} else {
+			res.pipe(output);
+		}
 
 		if (statusCode !== 304 && (statusCode < 200 || statusCode > 299)) {
 			proxy.emit('error', new got.HTTPError(statusCode, opts), null, res);
