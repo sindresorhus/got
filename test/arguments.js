0c9f3ca36dc6b604a16fbfe1cac7051dbc5b02ac
Luigi Pinca <luigipinca@gmail.com>
2017-11-10 09:52:50 +0100

    Do not throw when using a URL object with auth
    Reverts a breaking change introduced in #329.

--- test/arguments.js
+++ test/arguments.js
@@ -53,9 +53,18 @@ test('overrides querystring from opts', async t => {
 	t.is((await got(`${s.url}/?test=doge`, {query: {test: 'wow'}})).body, '/?test=wow');
 });
 
-test('should throw with auth in url', async t => {
+test('should throw with auth in url string', async t => {
 	const err = await t.throws(got('https://test:45d3ps453@account.myservice.com/api/token'));
 	t.regex(err.message, /Basic authentication must be done with auth option/);
+});
+
+test('does not throw with auth in url object', async t => {
+	await t.notThrows(got({
+		auth: 'foo:bar',
+		hostname: s.host,
+		port: s.port,
+		path: '/test'
+	}));
 });
 
 test('should throw when body is set to object', async t => {
@@ -65,13 +74,6 @@ test('should throw when body is set to object', async t => {
 test('WHATWG URL support', async t => {
 	const wURL = new URL(`${s.url}/test`);
 	await t.notThrows(got(wURL));
-});
-
-test('throws on WHATWG URL with auth', async t => {
-	const wURL = new URL(`${s.url}/test`);
-	wURL.username = 'alex';
-	wURL.password = 'secret';
-	await t.throws(got(wURL));
 });
 
 test.after('cleanup', async () => {

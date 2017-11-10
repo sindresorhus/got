sindresorhus/got

commit 3f117379e644cc3d097b496be8e15def25e9196a

`url` arg optimizations (#329)

index.js
@@ -10,6 +10,7 @@ const isStream = require('is-stream');
 const getStream = require('get-stream');
 const timedOut = require('timed-out');
 const urlParseLax = require('url-parse-lax');
+const urlToOptions = require('url-to-options');
 const lowercaseKeys = require('lowercase-keys');
 const decompressResponse = require('decompress-response');
 const isRetryAllowed = require('is-retry-allowed');
@@ -262,23 +263,15 @@ function asStream(opts) {
 function normalizeArguments(url, opts) {
 	if (typeof url !== 'string' && typeof url !== 'object') {
 		throw new TypeError(`Parameter \`url\` must be a string or object, not ${typeof url}`);
-	}
-
-	if (typeof url === 'string') {
+	} else if (typeof url === 'string') {
 		url = url.replace(/^unix:/, 'http://$&');
 		url = urlParseLax(url);
-
-		if (url.auth) {
-			throw new Error('Basic authentication must be done with auth option');
-		}
+	} else if (isURL.lenient(url)) {
+		url = urlToOptions(url);
 	}
 
-	if (isURL.lenient(url)) {
-		url = urlParseLax(url.href);
-
-		if (url.auth) {
-			throw new Error('Basic authentication must be done with auth option');
-		}
+	if (url.auth) {
+		throw new Error('Basic authentication must be done with auth option');
 	}
 
 	opts = Object.assign(

commit 0c9f3ca36dc6b604a16fbfe1cac7051dbc5b02ac

Do not throw when using a URL object with auth
Reverts a breaking change introduced in #329.

index.js
@@ -435,12 +435,11 @@ function normalizeArguments(url, opts) {
 	} else if (typeof url === 'string') {
 		url = url.replace(/^unix:/, 'http://$&');
 		url = urlParseLax(url);
+		if (url.auth) {
+			throw new Error('Basic authentication must be done with auth option');
+		}
 	} else if (isURL.lenient(url)) {
 		url = urlToOptions(url);
-	}
-
-	if (url.auth) {
-		throw new Error('Basic authentication must be done with auth option');
 	}
 
 	opts = Object.assign(

test/arguments.js
@@ -53,9 +53,18 @@ test('overrides querystring from opts', async t => {
 	t.is((await got(`${s.url}/?test=doge`, {query: {test: 'wow'}})).body, '/?test=wow');
 });
 
-test('should throw with auth in url', async t => {
+test('should throw with auth in url string', async t => {
 	const err = await t.throws(got('https://test:45d3ps453@account.myservice.com/api/token'));
 	t.regex(err.message, /Basic authentication must be done with auth option/);
+});
+
+test('does not throw with auth in url object', async t => {
+	await t.notThrows(got({
+		auth: 'foo:bar',
+		hostname: s.host,
+		port: s.port,
+		path: '/test'
+	}));
 });
 
 test('should throw when body is set to object', async t => {
@@ -65,13 +74,6 @@ test('should throw when body is set to object', async t => {
 test('WHATWG URL support', async t => {
 	const wURL = new URL(`${s.url}/test`);
 	await t.notThrows(got(wURL));
-});
-
-test('throws on WHATWG URL with auth', async t => {
-	const wURL = new URL(`${s.url}/test`);
-	wURL.username = 'alex';
-	wURL.password = 'secret';
-	await t.throws(got(wURL));
 });
 
 test.after('cleanup', async () => {