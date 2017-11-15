27a03baaf0da00c7fff13016f31d14b24132be38
Luke Childs <lukechilds123@gmail.com>
2017-11-15 11:39:14 +0700

    Only wait 10ms for before canceling
    This consistently fails on Travis and locally

--- test/cancel.js
+++ test/cancel.js
@@ -41,7 +41,7 @@ test('cancel in-progress request', async t => {
 	setTimeout(() => {
 		p.cancel();
 		body.push(null);
-	}, 100);
+	}, 10);
 
 	await t.throws(p, PCancelable.CancelError);
 	await t.notThrows(helper.aborted, 'Request finished instead of aborting.');
@@ -60,7 +60,7 @@ test('cancel in-progress request with timeout', async t => {
 	setTimeout(() => {
 		p.cancel();
 		body.push(null);
-	}, 100);
+	}, 10);
 
 	await t.throws(p, PCancelable.CancelError);
 	await t.notThrows(helper.aborted, 'Request finished instead of aborting.');
