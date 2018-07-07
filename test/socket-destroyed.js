5392d27cc24cb8254407d14e4c458b1ffcf58584
Brandon Smith <brandon93s@gmail.com>
2018-07-06 23:44:45 -0400

    Improve `socket-destroyed` test reliability

--- test/socket-destroyed.js
+++ test/socket-destroyed.js
@@ -1,9 +1,14 @@
 import test from 'ava';
 import got from '../source';
 
+const {Timer} = process.binding('timer_wrap');
+
 test.serial('clear the progressInterval if the socket has been destroyed', async t => {
-	const handlesComingFromAVA = 2;
 	const err = await t.throws(got(`http://127.0.0.1:55555`, {retry: 0}));
-	t.is(process._getActiveHandles().length - handlesComingFromAVA, 2);
+	const progressIntervalTimer = process._getActiveHandles().filter(handle => {
+		// Check if the handle is a Timer that matches the `uploadEventFrequency` interval
+		return handle instanceof Timer && handle._list.msecs === 150;
+	});
+	t.is(progressIntervalTimer.length, 0);
 	t.is(err.code, 'ECONNREFUSED');
 });
