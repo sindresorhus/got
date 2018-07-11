0399e2d38261adb6d50fabd37e856a63621ee234
Szymon Marczak <36894700+szmarczak@users.noreply.github.com>
2018-07-11 13:07:53 +0200

    Fix typo

--- test/arguments.js
+++ test/arguments.js
@@ -97,8 +97,8 @@ test('should not allow stream and JSON option at the same time', async t => {
 });
 
 test('throws TypeError when `url` is passed as an option', async t => {
-	await t.throws(got('', {url: 'example.com'}), {instanceof: TypeError});
-	await t.throws(got({url: 'example.com'}), {instanceof: TypeError});
+	await t.throws(got('', {url: 'example.com'}), {instanceOf: TypeError});
+	await t.throws(got({url: 'example.com'}), {instanceOf: TypeError});
 });
 
 test.after('cleanup', async () => {
