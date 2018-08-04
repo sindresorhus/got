393cc752a1582fb68a19eb8cd9b8f2d463722f1f
U-DESKTOP-07IKGBD\Szymon Marczak <sz.marczak@gmail.com>
2018-08-04 17:07:48 +0200

    Make it work

--- source/create.js
+++ source/create.js
@@ -10,6 +10,38 @@ const deepFreeze = require('./deep-freeze');
 const next = options => options.stream ? asStream(options) : asPromise(options);
 const mergeOptions = (defaults, options = {}) => merge({}, defaults, options);
 
+const mergeInstances = (instances, methods = instances[0].defaults.methods) => {
+	for (const [n, instance] of instances.entries()) {
+		if (Reflect.has(instance.defaults, 'mergeable') && !instance.defaults.mergeable) {
+			throw new Error('Instance ' + n + ' is not mergeable.');
+		}
+	}
+
+	const handlers = instances.map(instance => instance.defaults.handler);
+	const size = instances.length - 1;
+
+	let options = {};
+	for (const instance of instances) {
+		options = mergeOptions(options, instance.defaults.options);
+	}
+
+	// eslint-disable-next-line no-use-before-define
+	return create({
+		methods,
+		options,
+		handler: (url, options, next) => {
+			let iteration = -1;
+
+			const iterate = options => {
+				iteration++;
+				return handlers[iteration](url, options, iteration === size ? next : iterate);
+			};
+
+			return iterate(options);
+		}
+	});
+};
+
 const create = defaults => {
 	defaults = merge({}, defaults);
 	if (!defaults.handler) {
@@ -32,45 +64,14 @@ const create = defaults => {
 		handler: defaults.handler
 	});
 
-	const merge = (instances, methods = instances[0].defaults.methods) => {
-		for (const [n, instance] of instances.entries()) {
-			if (Reflect.has(instance.defaults, 'mergeable') && !instance.defaults.mergeable) {
-				throw new Error('Instance ' + n + ' is not mergeable.');
-			}
-		}
-
-		const handlers = instances.map(instance => instance.defaults.handler);
-		const size = instances.length - 1;
-
-		let options = {};
-		for (const instance of instances) {
-			options = mergeOptions(options, instance.defaults.options);
-		}
-
-		return create({
-			methods,
-			options,
-			handler: (url, options, next) => {
-				let iteration = -1;
-
-				const iterate = options => {
-					iteration++;
-					return handlers[iteration](url, options, iteration === size ? next : iterate);
-				};
-
-				return iterate(options);
-			}
-		});
-	};
-
 	got.merge = (instances, methods) => {
 		// Single instance
 		if (is.function(instances)) {
-			return merge([got, instances], methods);
+			return mergeInstances([got, instances], methods);
 		}
 
 		// Many instances
-		return merge(instances, methods);
+		return mergeInstances(instances, methods);
 	};
 
 	got.stream = (url, options) => {
