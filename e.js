f7fbc76f8aab675521e4ed090d03c902de121e94
Szymon Marczak <sz.marczak@gmail.com>
2018-08-30 21:56:50 +0200

    Remove debug file

--- e.js
+++
@@ -1,7 +0,0 @@
-const {app} = require('electron');
-const got = require('.');
-
-app.on('ready', async () => {
-	const {body} = await got('http://oo.pl', {useElectronNet: true});
-	console.log(body);
-});
