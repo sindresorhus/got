222ce541f096c337dc55bd238f697d68f4dc95ac
Denis Malinochkin <mrmlnc@yandex-team.ru>
2018-09-21 21:08:55 +0300

    feat: pass body of the «failed» request to the HTTP error

--- source/errors.js
+++ source/errors.js
@@ -59,7 +59,7 @@ module.exports.ParseError = class extends GotError {
 };
 
 module.exports.HTTPError = class extends GotError {
-	constructor(statusCode, statusMessage, headers, opts) {
+	constructor(statusCode, statusMessage, headers, body, opts) {
 		if (statusMessage) {
 			statusMessage = statusMessage.replace(/\r?\n/g, ' ').trim();
 		} else {
@@ -70,6 +70,7 @@ module.exports.HTTPError = class extends GotError {
 		this.statusCode = statusCode;
 		this.statusMessage = statusMessage;
 		this.headers = headers;
+		this.body = body;
 	}
 };
 
