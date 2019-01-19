c3c71b0ac63ea6c54efb35623fe9ae88285b1ab5
Scott Cooper <scttcper@gmail.com>
2019-01-18 20:38:42 -0800

    convert progress.ts to typescript

--- source/progress.ts
+++ source/progress.ts
@@ -1,96 +1,95 @@
-'use strict';
-const {Transform} = require('stream');
+import { Transform } from 'stream';
+import { EventEmitter } from 'events';
 
-module.exports = {
-	download(response, emitter, downloadBodySize) {
-		let downloaded = 0;
+export function download(response: any, emitter: EventEmitter, downloadBodySize?: number) {
+	let downloaded = 0;
 
-		return new Transform({
-			transform(chunk, encoding, callback) {
-				downloaded += chunk.length;
+	return new Transform({
+		transform(chunk, encoding, callback) {
+			downloaded += chunk.length;
 
-				const percent = downloadBodySize ? downloaded / downloadBodySize : 0;
+			const percent = downloadBodySize ? downloaded / downloadBodySize : 0;
 
-				// Let `flush()` be responsible for emitting the last event
-				if (percent < 1) {
-					emitter.emit('downloadProgress', {
-						percent,
-						transferred: downloaded,
-						total: downloadBodySize
-					});
-				}
-
-				callback(null, chunk);
-			},
-
-			flush(callback) {
+			// Let `flush()` be responsible for emitting the last event
+			if (percent < 1) {
 				emitter.emit('downloadProgress', {
-					percent: 1,
+					percent,
 					transferred: downloaded,
 					total: downloadBodySize
 				});
-
-				callback();
 			}
-		});
-	},
 
-	upload(request, emitter, uploadBodySize) {
-		const uploadEventFrequency = 150;
-		let uploaded = 0;
-		let progressInterval;
+			callback(undefined, chunk);
+		},
+
+		flush(callback) {
+			emitter.emit('downloadProgress', {
+				percent: 1,
+				transferred: downloaded,
+				total: downloadBodySize
+			});
+
+			callback();
+		}
+	});
+}
+
+export function upload(request: any, emitter: EventEmitter, uploadBodySize?: number) {
+	const uploadEventFrequency = 150;
+	let uploaded = 0;
+	let progressInterval: NodeJS.Timeout;
+
+	emitter.emit('uploadProgress', {
+		percent: 0,
+		transferred: 0,
+		total: uploadBodySize
+	});
+
+	request.once('error', () => {
+		clearInterval(progressInterval);
+	});
+
+	request.once('response', () => {
+		clearInterval(progressInterval);
 
 		emitter.emit('uploadProgress', {
-			percent: 0,
-			transferred: 0,
+			percent: 1,
+			transferred: uploaded,
 			total: uploadBodySize
 		});
+	});
 
-		request.once('error', () => {
-			clearInterval(progressInterval);
-		});
+	request.once('socket', (socket: any) => {
+		const onSocketConnect = () => {
+			progressInterval = setInterval(() => {
+				const lastUploaded = uploaded;
+				/* istanbul ignore next: see #490 (occurs randomly!) */
+				const headersSize = request._header ? Buffer.byteLength(request._header) : 0;
+				uploaded = socket.bytesWritten - headersSize;
 
-		request.once('response', () => {
-			clearInterval(progressInterval);
+				// Don't emit events with unchanged progress and
+				// prevent last event from being emitted, because
+				// it's emitted when `response` is emitted
+				if (uploaded === lastUploaded || uploaded === uploadBodySize) {
+					return;
+				}
 
-			emitter.emit('uploadProgress', {
-				percent: 1,
-				transferred: uploaded,
-				total: uploadBodySize
-			});
-		});
+				emitter.emit('uploadProgress', {
+					percent: uploadBodySize ? uploaded / uploadBodySize : 0,
+					transferred: uploaded,
+					total: uploadBodySize
+				});
+			}, uploadEventFrequency);
+		};
 
-		request.once('socket', socket => {
-			const onSocketConnect = () => {
-				progressInterval = setInterval(() => {
-					const lastUploaded = uploaded;
-					/* istanbul ignore next: see #490 (occurs randomly!) */
-					const headersSize = request._header ? Buffer.byteLength(request._header) : 0;
-					uploaded = socket.bytesWritten - headersSize;
+		/* istanbul ignore next: hard to test */
+		if (socket.connecting) {
+			socket.once('connect', onSocketConnect);
+		} else if (socket.writable) {
+			// The socket is being reused from pool,
+			// so the connect event will not be emitted
+			onSocketConnect();
+		}
+	});
+}
 
-					// Don't emit events with unchanged progress and
-					// prevent last event from being emitted, because
-					// it's emitted when `response` is emitted
-					if (uploaded === lastUploaded || uploaded === uploadBodySize) {
-						return;
-					}
-
-					emitter.emit('uploadProgress', {
-						percent: uploadBodySize ? uploaded / uploadBodySize : 0,
-						transferred: uploaded,
-						total: uploadBodySize
-					});
-				}, uploadEventFrequency);
-			};
-
-			/* istanbul ignore next: hard to test */
-			if (socket.connecting) {
-				socket.once('connect', onSocketConnect);
-			} else if (socket.writable) {
-				// The socket is being reused from pool,
-				// so the connect event will not be emitted
-				onSocketConnect();
-			}
-		});
-	}
-};
