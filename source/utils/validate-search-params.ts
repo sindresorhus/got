ca9e3f082ecc686704869f04cf341162a2204d96
Wang Zhongliang <wangz@garena.com>
2019-01-21 16:55:37 +0800

    any -> unknown

--- source/utils/validate-search-params.ts
+++ source/utils/validate-search-params.ts
@@ -4,7 +4,7 @@ type SearchParams = {
 	[key: string]: string | number | boolean | null
 }
 
-const verify = (value: any, type: string) => {
+const verify = (value: unknown, type: string) => {
 	if (!is.string(value) && !is.number(value) && !is.boolean(value) && !is.null_(value)) {
 		throw new TypeError(`The \`searchParams\` ${type} '${value}' must be a string, number, boolean or null`);
 	}
