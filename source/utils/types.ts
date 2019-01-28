7fc810d4e443dbfe333920bfde9ec4318c702ae9
fnky <chrs@cbp.io>
2019-01-28 17:37:02 +0100

    Add extra line between types

--- source/utils/types.ts
+++ source/utils/types.ts
@@ -4,6 +4,7 @@ import {PCancelable} from 'p-cancelable';
 import {Hooks} from '../known-hook-events';
 
 export type Method = 'GET' | 'PUT' | 'HEAD' | 'DELETE' | 'OPTIONS' | 'TRACE' | 'get' | 'put' | 'head' | 'delete' | 'options' | 'trace';
+
 export interface Options extends RequestOptions {
 	hooks?: Partial<Hooks>;
 	decompress?: boolean;
