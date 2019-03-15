191cd60edbb383f94c9e054a03d84361d16bf46d
Szymon Marczak <36894700+szmarczak@users.noreply.github.com>
2019-03-15 16:30:53 +0100

    Update with-server.js

--- test/helpers/with-server.js
+++ test/helpers/with-server.js
@@ -1,13 +1,11 @@
 'use strict';
+const {URL} = require('url');
 const createTestServer = require('create-test-server');
-
-exports.host = 'localhost';
-const {host} = exports;
 
 exports.withServer = async (t, run) => {
 	const s = await createTestServer();
 
-	s.host = host;
+	s.host = (new URL(s.url)).host;
 
 	try {
 		await run(t, s);
