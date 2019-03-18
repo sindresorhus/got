d350109aa75fa83175123ec81cd316c8e351be43
Matthias Pfeil <matthias.pfeil@isr.de>
2019-03-18 10:39:55 +0100

    convert promise.js test

--- test/promise.js
+++ test/promise.js
@@ -2,30 +2,23 @@ import {ClientRequest} from 'http';
 import {Transform as TransformStream} from 'stream';
 import test from 'ava';
 import got from '../source';
-import {createServer} from './helpers/server';
+import {withServer} from './helpers/with-server';
 
-let s;
-
-test.before('setup', async () => {
-	s = await createServer();
-	s.on('/', (request, response) => {
+test('should emit request event as promise', withServer, async (t, s) => {
+	s.get('/', (request, response) => {
 		response.statusCode = 200;
 		response.end();
 	});
-	await s.listen(s.port);
-});
-
-test.after('cleanup', async () => {
-	await s.close();
-});
-
-test('should emit request event as promise', async t => {
 	await got(s.url).json().on('request', request => {
 		t.true(request instanceof ClientRequest);
 	});
 });
 
-test('should emit response event as promise', async t => {
+test('should emit response event as promise', withServer, async (t, s) => {
+	s.get('/', (request, response) => {
+		response.statusCode = 200;
+		response.end();
+	});
 	await got(s.url).json().on('response', response => {
 		t.true(response instanceof TransformStream);
 		t.true(response.readable);
