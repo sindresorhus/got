266f673d4eebef6cd937bd6959a206de845b7a2a
Matthias Pfeil <matthias.pfeil@isr.de>
2019-03-20 17:15:32 +0100

    convert helpers.js test

--- test/helpers.js
+++ test/helpers.js
@@ -1,29 +1,16 @@
 import test from 'ava';
 import got from '../source';
-import {createServer} from './helpers/server';
+import {withServer} from './helpers/with-server';
 
-let s;
-
-test.before('setup', async () => {
-	s = await createServer();
-
-	s.on('/', (request, response) => {
+test('promise mode', withServer, async (t, s) => {
+	s.get('/', (request, response) => {
 		response.end('ok');
 	});
-
-	s.on('/404', (request, response) => {
+	s.get('/404', (request, response) => {
 		response.statusCode = 404;
 		response.end('not found');
 	});
 
-	await s.listen(s.port);
-});
-
-test.after('cleanup', async () => {
-	await s.close();
-});
-
-test('promise mode', async t => {
 	t.is((await got.get(s.url)).body, 'ok');
 
 	const error = await t.throwsAsync(got.get(`${s.url}/404`));
