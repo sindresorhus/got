3ec7a91edf95d6ca8a9a66fb5242b36aa92dca27
Karim Alibhai <karim@alibhai.co>
2019-07-08 15:03:51 -0400

    Update: timed-out test helper to support types

--- test/helpers/timed-out.ts
+++ test/helpers/timed-out.ts
@@ -1,21 +1,51 @@
 import http = require('http');
 
 export function init() {
-    const requests: Array<() => void> = [];
+    const timeoutMap = new Map<string, Array<{
+        delay: number;
+        timeout: (delay: number, type: string) => void;
+    }>>();
 
     return {
         request(...args: any[]) {
             // @ts-ignore
             const req = http.request(...args);
-            req.setTimeout = (_, timeout) => {
-                requests.push(timeout);
-            }
+            req.addTimeout = (delay, fn, type) => {
+                const timeouts = timeoutMap.get(type) || [];
+                if (!timeoutMap.has(type)) {
+                    timeoutMap.set(type, timeouts);
+                }
+
+                const timeoutObject = {
+                    delay,
+                    timeout: fn,
+                };
+
+                timeouts.push(timeoutObject);
+                return () => {
+                    timeouts.splice(timeouts.indexOf(timeoutObject), 1);
+                };
+            };
             return req;
         },
-        forceTimeout() {
-            for (const timeout of requests) {
-                timeout()
+        tickTimers(ms: number) {
+            for (const [type, timeouts] of timeoutMap.entries()) {
+                for (const {delay, timeout} of timeouts) {
+                    if (ms >= delay) {
+                        timeout(delay, type)
+                    }
+                }
             }
-        }
+        },
+        forceTimeout(type: string) {
+            const timeouts = timeoutMap.get(type);
+            if (!timeouts) {
+                throw new Error(`No timeouts registered for: '${type}'`)
+            }
+
+            for (const {delay, timeout} of timeouts) {
+                timeout(delay, type)
+            }
+        },
     }
 }
