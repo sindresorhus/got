d65ec15e72f7e345c4219b9a8a5c8f6391078f1b
Szymon Marczak <36894700+szmarczak@users.noreply.github.com>
2019-07-11 21:42:03 +0200

    tests

--- test/unix-socket.ts
+++ test/unix-socket.ts
@@ -3,29 +3,27 @@ import test from 'ava';
 import got from '../source';
 import {withSocketServer} from './helpers/with-server';
 
+const okHandler = (_request, response) => {
+	response.end('ok');
+};
+
 if (process.platform !== 'win32') {
 	test('works', withSocketServer, async (t, server) => {
-		server.on('/', (_request, response) => {
-			response.end('ok');
-		});
+		server.on('/', okHandler);
 
 		const url = format('http://unix:%s:%s', server.socketPath, '/');
 		t.is((await got(url)).body, 'ok');
 	});
 
 	test('protocol-less works', withSocketServer, async (t, server) => {
-		server.on('/', (_request, response) => {
-			response.end('ok');
-		});
+		server.on('/', okHandler);
 
 		const url = format('unix:%s:%s', server.socketPath, '/');
 		t.is((await got(url)).body, 'ok');
 	});
 
 	test('address with : works', withSocketServer, async (t, server) => {
-		server.on('/foo:bar', (_request, response) => {
-			response.end('ok');
-		});
+		server.on('/foo:bar', okHandler);
 
 		const url = format('unix:%s:%s', server.socketPath, '/foo:bar');
 		t.is((await got(url)).body, 'ok');
@@ -36,5 +34,13 @@ if (process.platform !== 'win32') {
 			instanceOf: got.RequestError,
 			code: 'ENOTFOUND'
 		});
+	});
+
+	test('works when extending instances', withSocketServer, async (t, server) => {
+		server.on('/', okHandler);
+
+		const url = format('unix:%s:%s', server.socketPath, '/');
+		const instance = got.extend({prefixUrl: url});
+		t.is((await instance('')).body, 'ok');
 	});
 }
