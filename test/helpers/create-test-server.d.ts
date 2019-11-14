4f9f6a176abed864f024d92082e544b15132e418
Michael Mok <pmmmwh@gmail.com>
2019-11-15 03:11:22 +0800

    Add types for create-test-server and slow-stream

--- test/helpers/create-test-server.d.ts
+++ test/helpers/create-test-server.d.ts
@@ -1,3 +1,17 @@
 declare module 'create-test-server' {
-	export default function createTestServer(options: any): Promise<any>;
+	import {Express} from 'express';
+
+	export interface TestServer extends Express {
+		caCert: any;
+		port: number;
+		url: string;
+		sslPort: number;
+		sslUrl: string;
+
+		close(): Promise<void>;
+	}
+
+	function createTestServer(options: any): Promise<TestServer>;
+
+	export = createTestServer;
 }
