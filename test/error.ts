71d2d02e28241f51992a814dd9616c7c82c7a3a1
Szymon Marczak <36894700+szmarczak@users.noreply.github.com>
2019-12-14 19:03:08 +0100

    skip a test

--- test/error.ts
+++ test/error.ts
@@ -207,5 +207,5 @@ test.skip('the old stacktrace is recovered', async t => {
 
 	// The first `at get` points to where the error was wrapped,
 	// the second `at get` points to the real cause.
-	t.not(error.stack!.indexOf('at get'), error.stack!.lastIndexOf('at get'), error.stack);
+	t.not(error.stack!.indexOf('at get'), error.stack!.lastIndexOf('at get'));
 });
