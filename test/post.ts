89785bad76bcfb3bf4732b67e903ed99e50d1d06
Andrew Goode <andrewbgoode@gmail.com>
2020-01-20 11:30:14 -0500

    Add failing test for DELETE with request body (#1031)

--- test/post.ts
+++ test/post.ts
@@ -261,3 +261,17 @@ test('the `form` payload is not touched', withServer, async (t, server, got) => 
 		}
 	});
 });
+
+test.failing('DELETE method sends plain objects as JSON', withServer, async (t, server, got) => {
+	server.delete('/', (request, response) => {
+		// Not using streams here to avoid this unhandled rejection:
+		// Error [ERR_STREAM_PREMATURE_CLOSE]: Premature close
+		response.json(request.body);
+	});
+
+	const {body} = await got.delete({
+		json: {such: 'wow'},
+		responseType: 'json'
+	});
+	t.deepEqual(body, {such: 'wow'});
+});
