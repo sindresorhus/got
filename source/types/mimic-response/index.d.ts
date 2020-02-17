6074fa0c7d4268e6a8ab8cc333e2fc174ddc6881
Sindre Sorhus <sindresorhus@gmail.com>
2020-02-17 14:13:20 +0700

    Update dependencies

--- source/types/mimic-response/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'mimic-response' {
-	import {IncomingMessage} from 'http';
-	import {Transform as TransformStream} from 'stream';
-
-	declare function mimicResponse(input: IncomingMessage, output: TransformStream): void;
-
-	export = mimicResponse;
-}
