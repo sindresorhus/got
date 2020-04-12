04821b96767ccb09d56a7e685970a3e07e1a7921
Szymon Marczak <36894700+szmarczak@users.noreply.github.com>
2020-04-12 10:23:13 +0200

    Fix FormData type

--- source/core/utils/is-form-data.ts
+++ source/core/utils/is-form-data.ts
@@ -3,6 +3,7 @@ import {Readable} from 'stream';
 
 interface FormData extends Readable {
 	getBoundary(): string;
+	getLength(callback: (error: Error | null, length: number) => void): void;
 }
 
 export default (body: unknown): body is FormData => is.nodeStream(body) && is.function_((body as FormData).getBoundary);
