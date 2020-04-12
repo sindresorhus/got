be086dad404f1804944ab780c6f5b1d5a7f3a376
Szymon Marczak <36894700+szmarczak@users.noreply.github.com>
2020-04-12 10:19:36 +0200

    Make form-data a dev dependency

--- source/core/utils/is-form-data.ts
+++ source/core/utils/is-form-data.ts
@@ -1,4 +1,8 @@
-import FormData = require('form-data');
 import is from '@sindresorhus/is';
+import {Readable} from 'stream';
+
+interface FormData extends Readable {
+	getBoundary(): string;
+}
 
 export default (body: unknown): body is FormData => is.nodeStream(body) && is.function_((body as FormData).getBoundary);
