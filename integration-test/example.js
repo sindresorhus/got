d97f2baa2b307a7bea0dda1f5690605f9c58fb0f
Giovanni Minotti <giovanni@minotti.net>
2020-05-16 14:32:03 +0200

    Restore working example

--- integration-test/example.js
+++ integration-test/example.js
@@ -2,8 +2,10 @@ const got = require('../dist/source');
 
 (async () => {
 	try {
-		await got('https://httpstat.us/500');
+		await got('https://google.com');
+		console.log('Integration test success');
 	} catch {
+		console.log('Integration test fail');
 		process.exit(1); // eslint-disable-line unicorn/no-process-exit
 	}
 })();
