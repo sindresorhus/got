fbd88d0b470df62b8dc53dc8f2c5d03babeb1d60
Giovanni Minotti <giovanni@minotti.net>
2020-05-16 14:23:27 +0200

    Small test to check failure

--- integration-test/example.js
+++ integration-test/example.js
@@ -2,7 +2,7 @@ const got = require('../dist/source');
 
 (async () => {
 	try {
-		await got('https://google.com');
+		await got('https://httpstat.us/500');
 	} catch {
 		process.exit(1); // eslint-disable-line unicorn/no-process-exit
 	}
