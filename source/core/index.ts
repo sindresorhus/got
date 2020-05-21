2824ee6e11458c666f573be1b0ad179b23e36847
Michael Mok <pmmmwh@gmail.com>
2020-05-21 21:15:30 +0800

    Add typescript docs for core options

--- source/core/index.ts
+++ source/core/index.ts
@@ -96,9 +96,76 @@ export type BeforeRedirectHook = (options: NormalizedOptions, response: Response
 export type BeforeErrorHook = (error: RequestError) => Promisable<RequestError>;
 
 export interface Hooks {
+	/**
+		Called with plain [request options](#options), right before their normalization. This is especially useful in conjunction with [`got.extend()`](#instances) when the input needs custom handling.
+
+		__Note #1__: This hook must be synchronous!
+
+		__Note #2__: Errors in this hook will be converted into an instances of [`RequestError`](#got.requesterror).
+
+		__Note #3__: The options object may not have a `url` property. To modify it, use a `beforeRequest` hook instead.
+
+		@default []
+	*/
 	init?: InitHook[];
+
+	/**
+		Called with [normalized](source/core/index.ts) [request options](#options). Got will make no further changes to the request before it is sent. This is especially useful in conjunction with [`got.extend()`](#instances) when you want to create an API client that, for example, uses HMAC-signing.
+
+		@default []
+		*/
 	beforeRequest?: BeforeRequestHook[];
+
+	/**
+		Called with [normalized](source/core/index.ts) [request options](#options) and the redirect [response](#response). Got will make no further changes to the request. This is especially useful when you want to avoid dead sites.
+
+		@default []
+
+		@example
+		```
+		const got = require('got');
+
+		got('https://example.com', {
+			hooks: {
+				beforeRedirect: [
+					(options, response) => {
+						if (options.hostname === 'deadSite') {
+							options.hostname = 'fallbackSite';
+						}
+					}
+				]
+			}
+		});
+		```
+		*/
 	beforeRedirect?: BeforeRedirectHook[];
+
+	/**
+		Called with an `Error` instance. The error is passed to the hook right before it's thrown. This is especially useful when you want to have more detailed errors.
+
+		__Note__: Errors thrown while normalizing input options are thrown directly and not part of this hook.
+
+		@example
+		```
+		const got = require('got');
+
+		got('https://api.github.com/some-endpoint', {
+			hooks: {
+				beforeError: [
+					error => {
+						const {response} = error;
+						if (response && response.body) {
+							error.name = 'GitHubError';
+							error.message = `${response.body.message} (${response.statusCode})`;
+						}
+
+						return error;
+					}
+				]
+			}
+		});
+		```
+		*/
 	beforeError?: BeforeErrorHook[];
 }
 
@@ -127,43 +194,355 @@ interface RealRequestOptions extends https.RequestOptions {
 }
 
 export interface Options extends URLOptions {
+	/**
+		Custom request function. The main purpose of this is to [support HTTP2 using a wrapper](https://github.com/szmarczak/http2-wrapper).
+
+		@default http.request | https.request
+	  */
 	request?: RequestFunction;
+
+	/**
+		An object representing `http`, `https` and `http2` keys for [`http.Agent`](https://nodejs.org/api/http.html#http_class_http_agent), [`https.Agent`](https://nodejs.org/api/https.html#https_class_https_agent) and [`http2wrapper.Agent`](https://github.com/szmarczak/http2-wrapper#new-http2agentoptions) instance. This is necessary because a request to one protocol might redirect to another. In such a scenario, Got will switch over to the right protocol agent for you.
+
+		If a key is not present, it will default to a global agent.
+
+		@example
+		```
+		const got = require('got');
+		const HttpAgent = require('agentkeepalive');
+		const {HttpsAgent} = HttpAgent;
+
+		got('https://sindresorhus.com', {
+			agent: {
+				http: new HttpAgent(),
+				https: new HttpsAgent()
+			}
+		});
+		```
+		*/
 	agent?: Agents | false;
+
+	/**
+		Decompress the response automatically. This will set the `accept-encoding` header to `gzip, deflate, br` on Node.js 11.7.0+ or `gzip, deflate` for older Node.js versions, unless you set it yourself.
+
+		Brotli (`br`) support requires Node.js 11.7.0 or later.
+
+		If this is disabled, a compressed response is returned as a `Buffer`. This may be useful if you want to handle decompression yourself or stream the raw compressed data.
+
+		@default true
+		*/
 	decompress?: boolean;
+
+	/**
+		Milliseconds to wait for the server to end the response before aborting the request with [`got.TimeoutError`](#gottimeouterror) error (a.k.a. `request` property). By default, there's no timeout.
+
+		This also accepts an `object` with the following fields to constrain the duration of each phase of the request lifecycle:
+
+		- `lookup` starts when a socket is assigned and ends when the hostname has been resolved. Does not apply when using a Unix domain socket.
+		- `connect` starts when `lookup` completes (or when the socket is assigned if lookup does not apply to the request) and ends when the socket is connected.
+		- `secureConnect` starts when `connect` completes and ends when the handshaking process completes (HTTPS only).
+		- `socket` starts when the socket is connected. See [request.setTimeout](https://nodejs.org/api/http.html#http_request_settimeout_timeout_callback).
+		- `response` starts when the request has been written to the socket and ends when the response headers are received.
+		- `send` starts when the socket is connected and ends with the request has been written to the socket.
+		- `request` starts when the request is initiated and ends when the response's end event fires.
+		*/
 	timeout?: Delays | number;
+
+	/**
+		When specified, `prefixUrl` will be prepended to `url`. The prefix can be any valid URL, either relative or absolute.
+		A trailing slash `/` is optional - one will be added automatically.
+
+		__Note__: `prefixUrl` will be ignored if the `url` argument is a URL instance.
+
+		__Note__: Leading slashes in `input` are disallowed when using this option to enforce consistency and avoid confusion. For example, when the prefix URL is `https://example.com/foo` and the input is `/bar`, there's ambiguity whether the resulting URL would become `https://example.com/foo/bar` or `https://example.com/bar`. The latter is used by browsers.
+
+		__Tip__: Useful when used with [`got.extend()`](#custom-endpoints) to create niche-specific Got instances.
+
+		__Tip__: You can change `prefixUrl` using hooks as long as the URL still includes the `prefixUrl`. If the URL doesn't include it anymore, it will throw.
+
+		@example
+		```
+		const got = require('got');
+
+		(async () => {
+			await got('unicorn', {prefixUrl: 'https://cats.com'});
+			//=> 'https://cats.com/unicorn'
+
+			const instance = got.extend({
+				prefixUrl: 'https://google.com'
+			});
+
+			await instance('unicorn', {
+				hooks: {
+					beforeRequest: [
+						options => {
+							options.prefixUrl = 'https://cats.com';
+						}
+					]
+				}
+			});
+			//=> 'https://cats.com/unicorn'
+		})();
+		```
+		*/
 	prefixUrl?: string | URL;
+
+	/**
+		__Note #1__: The `body` option cannot be used with the `json` or `form` option.
+
+		__Note #2__: If you provide this option, `got.stream()` will be read-only.
+
+		__Note #3__: If you provide a payload with the `GET` or `HEAD` method, it will throw a `TypeError` unless the method is `GET` and the `allowGetBody` option is set to `true`.
+
+		__Note #4__: This option is not enumerable and will not be merged with the instance defaults.
+
+		The `content-length` header will be automatically set if `body` is a `string` / `Buffer` / `fs.createReadStream` instance / [`form-data` instance](https://github.com/form-data/form-data), and `content-length` and `transfer-encoding` are not manually set in `options.headers`.
+		*/
 	body?: string | Buffer | Readable;
+
+	/**
+		The form body is converted to a query string using [`(new URLSearchParams(object)).toString()`](https://nodejs.org/api/url.html#url_constructor_new_urlsearchparams_obj).
+
+		If the `Content-Type` header is not present, it will be set to `application/x-www-form-urlencoded`.
+
+		__Note #1__: If you provide this option, `got.stream()` will be read-only.
+
+		__Note #2__: This option is not enumerable and will not be merged with the instance defaults.
+		*/
 	form?: {[key: string]: any};
+
+	/**
+		JSON body. If the `Content-Type` header is not set, it will be set to `application/json`.
+
+		__Note #1__: If you provide this option, `got.stream()` will be read-only.
+
+		__Note #2__: This option is not enumerable and will not be merged with the instance defaults.
+		*/
 	json?: {[key: string]: any};
+
 	url?: string | URL;
+
+	/**
+		Cookie support. You don't have to care about parsing or how to store them.
+
+		__Note__: If you provide this option, `options.headers.cookie` will be overridden.
+		*/
 	cookieJar?: PromiseCookieJar | ToughCookieJar;
+
+	/**
+		Ignore invalid cookies instead of throwing an error. Only useful when the `cookieJar` option has been set. Not recommended.
+
+		@default false
+		*/
 	ignoreInvalidCookies?: boolean;
+
+	/**
+		Query string that will be added to the request URL. This will override the query string in `url`.
+
+		If you need to pass in an array, you can do it using a `URLSearchParams` instance.
+
+		@example
+		```
+		const got = require('got');
+
+		const searchParams = new URLSearchParams([['key', 'a'], ['key', 'b']]);
+
+		got('https://example.com', {searchParams});
+
+		console.log(searchParams.toString());
+		//=> 'key=a&key=b'
+		```
+		*/
 	searchParams?: string | {[key: string]: string | number | boolean | null} | URLSearchParams;
+
+	/**
+		An instance of [`CacheableLookup`](https://github.com/szmarczak/cacheable-lookup) used for making DNS lookups. Useful when making lots of requests to different *public* hostnames.
+
+		__Note__: This should stay disabled when making requests to internal hostnames such as `localhost`, `database.local` etc.
+
+		`CacheableLookup` uses `dns.resolver4(..)` and `dns.resolver6(...)` under the hood and fall backs to `dns.lookup(...)` when the first two fail, which may lead to additional delay.
+
+		@default false
+		*/
 	dnsCache?: CacheableLookup | boolean;
+
+	/**
+		User data. In contrast to other options, `context` is not enumerable.
+
+		__Note__: The object is never merged, it's just passed through. Got will not modify the object in any way.
+
+		@example
+		```
+		const got = require('got');
+
+		const instance = got.extend({
+			hooks: {
+				beforeRequest: [
+					options => {
+						if (!options.context || !options.context.token) {
+							throw new Error('Token required');
+						}
+
+						options.headers.token = options.context.token;
+					}
+				]
+			}
+		});
+
+		(async () => {
+			const context = {
+				token: 'secret'
+			};
+
+			const response = await instance('https://httpbin.org/headers', {context});
+
+			// Let's see the headers
+			console.log(response.body);
+		})();
+		```
+		*/
 	context?: object;
+
+	/**
+		Hooks allow modifications during the request lifecycle. Hook functions may be async and are run serially.
+		*/
 	hooks?: Hooks;
+
+	/**
+		Defines if redirect responses should be followed automatically.
+
+		Note that if a `303` is sent by the server in response to any request type (`POST`, `DELETE`, etc.), Got will automatically request the resource pointed to in the location header via `GET`.
+		This is in accordance with [the spec](https://tools.ietf.org/html/rfc7231#section-6.4.4).
+
+		@default true
+		*/
 	followRedirect?: boolean;
+
+	/**
+		If exceeded, the request will be aborted and a `MaxRedirectsError` will be thrown.
+
+		@default 10
+		*/
 	maxRedirects?: number;
+
+	/**
+		[Cache adapter instance](#cache-adapters) for storing cached response data.
+
+		@default false
+		*/
 	cache?: string | CacheableRequest.StorageAdapter | false;
+
+	/**
+		Determines if a [`got.HTTPError`](#gothttperror) is thrown for unsuccessful responses.
+
+		If this is disabled, requests that encounter an error status code will be resolved with the `response` instead of throwing. This may be useful if you are checking for resource availability and are expecting error responses.
+
+		@default true
+		*/
 	throwHttpErrors?: boolean;
+
 	username?: string;
+
 	password?: string;
+
+	/**
+		If set to `true`, Got will additionally accept HTTP2 requests.
+
+		It will choose either HTTP/1.1 or HTTP/2 depending on the ALPN protocol.
+
+		__Note__: Overriding `options.request` will disable HTTP2 support.
+
+		__Note__: This option will default to `true` in the next upcoming major release.
+
+		@default false
+
+		@example
+		```
+		const got = require('got');
+
+		(async () => {
+			const {headers} = await got('https://nghttp2.org/httpbin/anything', {http2: true});
+			console.log(headers.via);
+			//=> '2 nghttpx'
+		})();
+		```
+	*/
 	http2?: boolean;
+
+	/**
+		Set this to `true` to allow sending body for the `GET` method. However, the [HTTP/2 specification](https://tools.ietf.org/html/rfc7540#section-8.1.3) says that `An HTTP GET request includes request header fields and no payload body`, therefore when using the HTTP/2 protocol this option will have no effect. This option is only meant to interact with non-compliant servers when you have no other choice.
+
+		__Note__: The [RFC 7321](https://tools.ietf.org/html/rfc7231#section-4.3.1) doesn't specify any particular behavior for the GET method having a payload, therefore __it's considered an [anti-pattern](https://en.wikipedia.org/wiki/Anti-pattern)__.
+
+		@default false
+		*/
 	allowGetBody?: boolean;
+
 	lookup?: CacheableLookup['lookup'];
+
+	/**
+		Request headers.
+
+		Existing headers will be overwritten. Headers set to `undefined` will be omitted.
+
+		@default {}
+	 */
 	headers?: Headers;
+
+	/**
+		By default, redirects will use [method rewriting](https://tools.ietf.org/html/rfc7231#section-6.4).
+		For example, when sending a POST request and receiving a `302`, it will resend the body to the new location using the same HTTP method (`POST` in this case).
+
+		@default true
+		*/
 	methodRewriting?: boolean;
 	dnsLookupIpVersion?: DnsLookupIpVersion;
 	parseJson?: ParseJsonFunction;
 	stringifyJson?: StringifyJsonFunction;
 
 	// From `http.RequestOptions`
+	/**
+		The IP address used to send the request from.
+		*/
 	localAddress?: string;
+
 	socketPath?: string;
+
+	/**
+		The HTTP method used to make the request.
+
+		@default 'GET'
+		*/
 	method?: Method;
+
 	createConnection?: (options: http.RequestOptions, oncreate: (error: Error, socket: Socket) => void) => Socket;
 
 	// TODO: remove when Got 12 gets released
+	/**
+		If set to `false`, all invalid SSL certificates will be ignored and no error will be thrown.
+
+		If set to `true`, it will throw an error whenever an invalid SSL certificate is detected.
+
+		We strongly recommend to have this set to `true` for security reasons.
+
+		@default true
+
+		@example
+		```
+		const got = require('got');
+
+		(async () => {
+			// Correct:
+			await got('https://example.com', {rejectUnauthorized: true});
+
+			// You can disable it when developing an HTTPS app:
+			await got('https://localhost', {rejectUnauthorized: false});
+
+			// Never do this:
+			await got('https://example.com', {rejectUnauthorized: false});
+		})();
+		```
+	  */
 	rejectUnauthorized?: boolean; // Here for backwards compatibility
 
 	https?: HTTPSOptions;
