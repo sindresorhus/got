47cb3d1f5c0844f1a429c623571c02ec5c5c92fa
Michael Mok <pmmmwh@gmail.com>
2020-05-21 21:21:35 +0800

    Add typescript docs for core response

--- source/core/index.ts
+++ source/core/index.ts
@@ -630,20 +630,90 @@ export interface Progress {
 }
 
 export interface PlainResponse extends IncomingMessageWithTimings {
+	/**
+		The original request URL.
+		*/
 	requestUrl: string;
+
+	/**
+		The redirect URLs.
+		*/
 	redirectUrls: string[];
+
+	/**
+		__Note__: This is not a [http.ClientRequest](https://nodejs.org/api/http.html#http_class_http_clientrequest).
+
+		- `options` - The Got options that were set on this request.
+		*/
 	request: Request;
+
+	/**
+		The remote IP address.
+
+		__Note__: Not available when the response is cached. This is hopefully a temporary limitation, see [lukechilds/cacheable-request#86](https://github.com/lukechilds/cacheable-request/issues/86).
+		*/
 	ip?: string;
+
+	/**
+		Whether the response was retrieved from the cache.
+		*/
 	isFromCache: boolean;
+
+	/**
+		The status code of the response.
+		*/
 	statusCode: number;
+
+	/**
+		The request URL or the final URL after redirects.
+		*/
 	url: string;
+
+	/**
+		The object contains the following properties:
+
+		- `start` - Time when the request started.
+		- `socket` - Time when a socket was assigned to the request.
+		- `lookup` - Time when the DNS lookup finished.
+		- `connect` - Time when the socket successfully connected.
+		- `secureConnect` - Time when the socket securely connected.
+		- `upload` - Time when the request finished uploading.
+		- `response` - Time when the request fired `response` event.
+		- `end` - Time when the response fired `end` event.
+		- `error` - Time when the request fired `error` event.
+		- `abort` - Time when the request fired `abort` event.
+		- `phases`
+			- `wait` - `timings.socket - timings.start`
+			- `dns` - `timings.lookup - timings.socket`
+			- `tcp` - `timings.connect - timings.lookup`
+			- `tls` - `timings.secureConnect - timings.connect`
+			- `request` - `timings.upload - (timings.secureConnect || timings.connect)`
+			- `firstByte` - `timings.response - timings.upload`
+			- `download` - `timings.end - timings.response`
+			- `total` - `(timings.end || timings.error || timings.abort) - timings.start`
+
+		If something has not been measured yet, it will be `undefined`.
+
+		__Note__: The time is a `number` representing the milliseconds elapsed since the UNIX epoch.
+		*/
 	timings: Timings;
 }
 
 // For Promise support
 export interface Response<T = unknown> extends PlainResponse {
+	/**
+		The result of the request.
+		*/
 	body: T;
+
+	/**
+		The raw result of the request.
+		*/
 	rawBody: Buffer;
+
+	/**
+		The number of times the request was retried.
+		*/
 	retryCount: number;
 }
 
