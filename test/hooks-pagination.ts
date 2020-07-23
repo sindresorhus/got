1690eb69a780afd9017e92781441b151b2031688
David Wu <wu.david3283@gmail.com>
2020-07-23 16:54:12 +1000

    add tests for duplicate hook calls in pagination

--- test/hooks-pagination.ts
+++ test/hooks-pagination.ts
@@ -1,9 +1,9 @@
 import test from 'ava';
 import withServer from './helpers/with-server';
 
-test('no duplicate hooks in paginated requests', withServer, async (t, server, got) => {
+test('no duplicate hook calls in single-page paginated requests', withServer, async (t, server, got) => {
 	server.get('/get', (_request, response) => {
-		response.end('i <3 unicorns');
+		response.end('i <3 koalas');
 	});
 
 	let beforeHookCount = 0;
@@ -32,8 +32,73 @@ test('no duplicate hooks in paginated requests', withServer, async (t, server, g
 		}
 	});
 
-	await instance.paginate.all('/get');
+	await instance.paginate.all('get');
 
 	t.is(beforeHookCount, 1);
 	t.is(afterHookCount, 1);
+
+	await got.paginate.all('get', {
+		hooks,
+		pagination: {
+			paginate: () => false,
+			countLimit: 2009,
+			transform: response => [response]
+		}
+	});
+
+	t.is(beforeHookCount, 2);
+	t.is(afterHookCount, 2);
+});
+
+test('no duplicate hook calls in sequential paginated requests', withServer, async (t, server, got) => {
+	server.get('/get', (_request, response) => {
+		response.end('i <3 unicorns');
+	});
+
+	let reqNum = 0;
+	let beforeHookCount = 0;
+	let afterHookCount = 0;
+
+	const hooks = {
+		beforeRequest: [
+			() => {
+				beforeHookCount++;
+			}
+		],
+		afterResponse: [
+			(response: any) => {
+				afterHookCount++;
+				return response;
+			}
+		]
+	};
+
+	const paginate = () => reqNum++ === 0 ? {} : false;
+
+	const instance = got.extend({
+		hooks,
+		pagination: {
+			paginate,
+			countLimit: 2009,
+			transform: response => [response]
+		}
+	});
+
+	await instance.paginate.all('get');
+
+	t.is(beforeHookCount, 2);
+	t.is(afterHookCount, 2);
+	reqNum = 0;
+
+	await got.paginate.all('get', {
+		hooks,
+		pagination: {
+			paginate,
+			countLimit: 2009,
+			transform: response => [response]
+		}
+	});
+
+	t.is(beforeHookCount, 4);
+	t.is(afterHookCount, 4);
 });
