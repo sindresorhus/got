82caab2c799b017fb0c49d7d9adf660bedd4abe0
Szymon Marczak <36894700+szmarczak@users.noreply.github.com>
2020-08-07 01:32:15 +0200

    Export `isErrorWithResponse`

--- source/core/calculate-retry-delay.ts
+++ source/core/calculate-retry-delay.ts
@@ -12,7 +12,7 @@ type Returns<T extends (...args: any) => unknown, V> = (...args: Parameters<T>) 
 
 const retryAfterStatusCodes: ReadonlySet<number> = new Set([413, 429, 503]);
 
-const isErrorWithResponse = (error: RetryObject['error']): error is HTTPError | ParseError | MaxRedirectsError => (
+export const isErrorWithResponse = (error: RetryObject['error']): error is HTTPError | ParseError | MaxRedirectsError => (
 	error instanceof HTTPError || error instanceof ParseError || error instanceof MaxRedirectsError
 );
 
