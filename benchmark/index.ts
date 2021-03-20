a74864a961f18c16de7a22bbc49be99c2497ba2f
Szymon Marczak <36894700+szmarczak@users.noreply.github.com>
2021-03-20 03:35:52 +0100

    benchmark got

--- benchmark/index.ts
+++ benchmark/index.ts
@@ -1,20 +1,26 @@
+import * as https from 'https';
 import * as Benchmark from 'benchmark';
-import Options, { OptionsInit } from '../source/core/options';
+import Request from '../source/core';
+import Options from '../source/core/options';
 
-const x: OptionsInit = {
-	hooks: {
-		beforeRequest: [
-			() => {}
-		]
-	}
-};
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-const y = new Options(x);
+// @ts-expect-error
+https.globalAgent.keepAlive = true;
+
+const options = new Options({
+	url: 'https://127.0.0.1:8080'
+});
 
 const internalSuite = new Benchmark.Suite();
-internalSuite.add('got - normalize options', {
-	fn: () => {
-		new Options(y);
+internalSuite.add('got', {
+	defer: true,
+	fn: (deferred: Benchmark.Deferred) => {
+		const request = new Request(undefined, options);
+		request.resume();
+		request.once('end', () => {
+			deferred.resolve();
+		});
 	}
 }).on('cycle', (event: Benchmark.Event) => {
 	console.log(String(event.target));
