1e95380f2870522489de95c7cad183e4887af48a
Szymon Marczak <36894700+szmarczak@users.noreply.github.com>
2021-03-29 00:54:29 +0200

    Handler are amazing!

--- demo.js
+++ demo.js
@@ -1,6 +1,23 @@
 const got = require('./dist/source').default;
 
+const instance = got.extend({
+	handlers: [
+		(options, next) => {
+			if (options.isStream) {
+				return next(options);
+			}
+
+			const promise = next(options);
+			promise.getLength = async () => {
+				return (await promise).body.length;
+			};
+
+			return promise;
+		}
+	]
+});
+
 (async () => {
-	const response = await got('https://httpbin.org/anything');
-	console.log(response.body);
+	const response = await instance('https://httpbin.org/anything').getLength();
+	console.log(response);
 })();
