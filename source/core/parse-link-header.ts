a09ac7ca1da7908d358d3bad49415cad890ec3ac
Szymon Marczak <36894700+szmarczak@users.noreply.github.com>
2021-04-09 23:41:06 +0200

    Test link header

--- source/core/parse-link-header.ts
+++ source/core/parse-link-header.ts
@@ -5,19 +5,30 @@ export default function parseLinkHeader(link: string) {
 
 	for (const item of items) {
 		// https://tools.ietf.org/html/rfc5988#section-5
-		const [rawUriReference, ...rawLinkParameters] = item.split(';');
+		const [rawUriReference, ...rawLinkParameters] = item.split(';') as [string, ...string[]];
+		const trimmedUriReference = rawUriReference.trim();
 
-		const reference = rawUriReference!.trim().slice(1, -1);
+		if (trimmedUriReference[0] !== '<' || trimmedUriReference[trimmedUriReference.length - 1] !== '>') {
+			throw new Error(`Invalid format of the Link header reference: ${trimmedUriReference}`);
+		}
+
+		const reference = trimmedUriReference.slice(1, -1);
 		const parameters: Record<string, string> = {};
 
 		if (rawLinkParameters.length === 0) {
-			throw new Error('Unexpected end of Link header parameters');
+			throw new Error(`Unexpected end of Link header parameters: ${rawLinkParameters.join(';')}`);
 		}
 
 		for (const rawParameter of rawLinkParameters) {
-			const center = rawParameter.indexOf('=');
-			const name = rawParameter.slice(0, center).trim();
-			const value = rawParameter.slice(center + 1).trim();
+			const trimmedRawParameter = rawParameter.trim();
+			const center = trimmedRawParameter.indexOf('=');
+
+			if (center === -1) {
+				throw new Error(`Failed to parse Link header: ${link}`);
+			}
+
+			const name = trimmedRawParameter.slice(0, center).trim();
+			const value = trimmedRawParameter.slice(center + 1).trim();
 
 			parameters[name] = value;
 		}
