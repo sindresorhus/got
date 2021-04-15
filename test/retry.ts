2649ada828ea870b6ac91a8c4283d4ae7fdb524c
Szymon Marczak <36894700+szmarczak@users.noreply.github.com>
2021-04-16 00:15:04 +0200

    Increase window when testing `backoffLimit`

--- test/retry.ts
+++ test/retry.ts
@@ -593,7 +593,7 @@ test('respects backoffLimit', withServer, async (t, server, got) => {
 	}).json<number[]>();
 
 	t.is(body.length, 3);
-	t.true(body[0]! < 200);
-	t.true(body[1]! < 200);
-	t.true(body[2]! < 200);
+	t.true(body[0]! < 400);
+	t.true(body[1]! < 400);
+	t.true(body[2]! < 400);
 });
