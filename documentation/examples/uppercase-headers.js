fee9685fdd288d5f920377bc15c0bd83bec97b80
Szymon Marczak <36894700+szmarczak@users.noreply.github.com>
2021-08-02 08:23:46 +0200

    Covert spaces to tabs in an example

--- documentation/examples/uppercase-headers.js
+++ documentation/examples/uppercase-headers.js
@@ -3,55 +3,55 @@ import got from '../../dist/source/index.js';
 
 // Wraps an existing Agent instance
 class WrappedAgent {
-    constructor(agent) {
-        this.agent = agent;
-    }
+	constructor(agent) {
+		this.agent = agent;
+	}
 
-    addRequest(request, options) {
-        return this.agent.addRequest(request, options);
-    }
+	addRequest(request, options) {
+		return this.agent.addRequest(request, options);
+	}
 
-    get keepAlive() {
-        return this.agent.keepAlive;
-    }
+	get keepAlive() {
+		return this.agent.keepAlive;
+	}
 
-    get maxSockets() {
-        return this.agent.maxSockets;
-    }
+	get maxSockets() {
+		return this.agent.maxSockets;
+	}
 
-    get options() {
-        return this.agent.options;
-    }
+	get options() {
+		return this.agent.options;
+	}
 
-    get defaultPort() {
-        return this.agent.defaultPort;
-    }
+	get defaultPort() {
+		return this.agent.defaultPort;
+	}
 
-    get protocol() {
-        return this.agent.protocol;
-    }
+	get protocol() {
+		return this.agent.protocol;
+	}
 }
 
 class TransformHeadersAgent extends WrappedAgent {
-    addRequest(request, options) {
-        const headers = request.getHeaderNames();
+	addRequest(request, options) {
+		const headers = request.getHeaderNames();
 
-        for (const header of headers) {
-            request.setHeader(this.transformHeader(header), request.getHeader(header));
-        }
+		for (const header of headers) {
+			request.setHeader(this.transformHeader(header), request.getHeader(header));
+		}
 
-        return super.addRequest(request, options);
-    }
+		return super.addRequest(request, options);
+	}
 
-    transformHeader(header) {
-        return header.split('-').map(part => {
-            return part[0].toUpperCase() + part.slice(1);
-        }).join('-');
-    }
+	transformHeader(header) {
+		return header.split('-').map(part => {
+			return part[0].toUpperCase() + part.slice(1);
+		}).join('-');
+	}
 }
 
 const agent = new http.Agent({
-    keepAlive: true
+	keepAlive: true
 });
 
 const wrappedAgent = new TransformHeadersAgent(agent);
