7570056b67159e502b0c5434929783b7935ecd76
Etienne Brodu <etienne.brodu@datadoghq.com>
2022-01-04 17:26:19 +0100

    Add http/1 and http/2 timings tests

---
+++ test/timings.ts
@@ -0,0 +1,58 @@
+import test from 'ava';
+import got from '../source/index.js';
+
+test('http/1 timings', async t => {
+	const request = await got('https://httpbin.org/anything');
+
+	const {timings} = request;
+
+	t.true(timings.start >= 0);
+	t.true(timings.socket! >= 0);
+	t.true(timings.lookup! >= 0);
+	t.true(timings.connect! >= 0);
+	t.true(timings.secureConnect! >= 0);
+	t.true(timings.upload! >= 0);
+	t.true(timings.response! >= 0);
+	t.true(timings.end! >= 0);
+
+	const {phases} = timings;
+
+	t.true(phases.wait! >= 0);
+	t.true(phases.dns! >= 0);
+	t.true(phases.tcp! >= 0);
+	t.true(phases.tls! >= 0);
+	t.true(phases.request! >= 0);
+	t.true(phases.firstByte! >= 0);
+	t.true(phases.download! >= 0);
+	t.true(phases.total! >= 0);
+
+	t.pass();
+});
+
+test('http/2 timings', async t => {
+	const request = await got('https://httpbin.org/anything', {http2: true});
+
+	const {timings} = request;
+
+	t.true(timings.start >= 0);
+	t.true(timings.socket! >= 0);
+	t.true(timings.lookup! >= 0);
+	t.true(timings.connect! >= 0);
+	t.true(timings.secureConnect! >= 0);
+	t.true(timings.upload! >= 0);
+	t.true(timings.response! >= 0);
+	t.true(timings.end! >= 0);
+
+	const {phases} = timings;
+
+	t.true(phases.wait! >= 0);
+	t.true(phases.dns! >= 0);
+	t.true(phases.tcp! >= 0);
+	t.true(phases.tls! >= 0);
+	t.true(phases.request! >= 0);
+	t.true(phases.firstByte! >= 0);
+	t.true(phases.download! >= 0);
+	t.true(phases.total! >= 0);
+
+	t.pass();
+});
