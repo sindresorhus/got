17befb7bd140effd385fa5b7efb92b90355d9f38
Baptiste Marchand <baptiste.marchand@ambler.fr>
2022-02-28 15:45:09 +0100

    Fix encoding with json `responseType`

---
+++ test/encoding.ts
@@ -0,0 +1,19 @@
+import {Buffer} from 'buffer';
+import test from 'ava';
+import withServer from './helpers/with-server.js';
+
+test('encoding works with json', withServer, async (t, server, got) => {
+	const json = {data: 'é'};
+
+	server.get('/', (_request, response) => {
+		response.set('Content-Type', 'application-json');
+		response.send(Buffer.from(JSON.stringify(json), 'latin1'));
+	});
+
+	const response = await got('', {
+		encoding: 'latin1',
+		responseType: 'json',
+	});
+
+	t.deepEqual(response.body, json);
+});
