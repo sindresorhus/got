79ede3d32a9c8d91c1996150beca99ddc07ed048
Sindre Sorhus <sindresorhus@gmail.com>
2023-10-29 14:49:54 +0700

    Update options.ts

--- source/core/options.ts
+++ source/core/options.ts
@@ -1756,8 +1756,9 @@ export default class Options {
 	}
 
 	/**
-	Defines if redirect responses should be followed automatically. Defaults to `true`.
-	This property can also be implemented as a function which gets response object as a single argument and should return true if redirects should continue or false otherwise.
+	Whether redirect responses should be followed automatically.
+
+ 	Optionally, pass a function to dynamically decide based on the response object.
 
 	Note that if a `303` is sent by the server in response to any request type (`POST`, `DELETE`, etc.), Got will automatically request the resource pointed to in the location header via `GET`.
 	This is in accordance with [the spec](https://tools.ietf.org/html/rfc7231#section-6.4.4). You can optionally turn on this behavior also for other redirect codes - see `methodRewriting`.
